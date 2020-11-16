8f8ed4254641727a30641b7a6a16a03fa99e953b
adipie <adipiek@gmail.com>
2020-11-16 10:18:51 +0200

    read about data from object

---
+++ src/pages/about-data.js
@@ -0,0 +1,14 @@
+export const aboutData = {
+    subHeader: "I'm Adi, a software engineer from Givatayim. \
+        In addition to my passion for technology and math, I also raise two dogs and participate in various dog sports.", 
+    paragraph1: "Around the 8th grade I discovered my love for math and knew this will be my direction going forward. \
+        I started working in the tech industry after eceiving my B.Sc. in electrical engineering in 2013. \
+        My journey began at a small start-up called 'Matific' as a manual QA Engineer. \
+        During my time there I learned a lot about software development and architecture of web applications. \
+        I developed automated tests in Java and selenium, and learned HTML and CSS in the process. \
+        By the time I left the company I was a python automation developer, analyzing data with pandas and managing my own machines on AWS.", 
+    paragraph2: "My next role was a full-stack developer in Bitmain. It was significant though short as the Israeli branch of Bitmain closed only a few months after I started. \
+        This period in my career added a few new things to my toolbox and led me to mentoring in node girls workshop.",
+    paragraph13: "In my last position I was a python developer at Arilou, a company that develops automotive cyber security solutions. \
+        In addition to regular day to day tasks I also had demo projects and the opportunity to lead a clean code workshop."
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import profile from '../images/profile.jpg'
-import SectionTitle from '../components/section_title'
-import Skills from '../components/Skills'
-import './About.css'
+import profile from '../images/profile.jpg';
+import SectionTitle from '../components/section_title';
+import Skills from '../components/Skills';
+import { aboutData } from './about-data';
+import './About.css';
 
 function About() {
+    const subHeader = () => {
+        return <p className="sub_header content">{ aboutData.subHeader }</p>
+    }
+
+    const paragraphs = () => {
+        return Object.entries(aboutData).map(([key, value], i) => {
+            if (key != "subHeader") {
+                return <p className="content">{ value }</p>;
+            }
+        });
+    }
+
     return <div className="about_content">
                 <div id="left">
                     <img src={profile} alt="profile pic" id="profile_pic" />
@@ -15,20 +28,8 @@ function About() {
                 </div>
                 <div id="right">
                     <SectionTitle top="personal & proffesional" bottom="background" /> 
-                    <p className="sub_header content">I'm Adi, a software engineer from Givatayim, mostly experienced in the web development area. 
-                    In addition to my passion for technology and math, I also raise two dogs and participate in various dog sports.</p>
-                    <p className="content">Around the 8th grade I discovered my love for math and knew this will be my direction going forward. 
-                        I started working in the tech industry after I graduated my studies of electrical engineering in 2013. My journey began at a small start up called "Matific" 
-                        as a manual QA Engineer. During my time there I learned a lot about development and architecture of web applications. I was developing automated tests 
-                        in Java and selenium, learning html and css in the process. By the time I left there I was a python automation developer, analyzing data with pandas and 
-                        managing my own machines on AWS.
-
-                    </p>
-                    <p className="content">
-                    My first formal development role was significant though short as the Israeli branch of Bitmain closed only a few months after I started. This period in my career added a few new things to my toolbox and led me to mentoring in node girls workshop.
-                        My last position was at Arilou, developing automotive cyber security solutions, where I was working as a python developer. In addition to regular day to day tasks I also had demo projects and the opportunity to host a clean code workshop. 
-                        I lost my job during the covid-19 crisis and this website is my way to keep learning and improving while searching for my next job. Future plans for my website is to add a blog, telling both about my tech journey and the life as a dog handler. 
-                    </p>
+                    { subHeader() }
+                    { paragraphs()}
                 </div>
         </div>
 }
